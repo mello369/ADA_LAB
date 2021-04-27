2d56dcf985f63cfa5f8b255810b007cfbd26ebd1
Naman Singh <55326403+mello369@users.noreply.github.com>
2021-04-27 15:04:34 +0530

    Update gcd.c

--- week1/a/gcd.c
+++ week1/a/gcd.c
@@ -1,1 +1,21 @@
 
+
+#include <stdio.h>
+
+int gcd(int a,int b)
+{
+    if(a==0)
+    return b;
+    return gcd(b%a,a);
+}
+int main()
+{
+    int a,b,g;
+    printf("Enter 1st number : \n");
+    scanf("%d",&a);
+    printf("\nEnter second number : \n");
+    scanf("%d",&b);
+    g = a<b?gcd(a,b):gcd(b,a);
+    printf("GCD : %d",g);
+    return 0;
+}
